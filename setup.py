ce85d1c5d0eb55e87ec1787184b3ba957811f611
j08lue <j08lue@gmail.com>
2015-12-14 14:56:08 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name = 'pygeo',
-      version = '0.1.0',
+      version = '0.2.0',
       author = 'Jonas Bluethgen',
       author_email = 'bluthgen@nbi.ku.dk',
       packages = ['pygeo'],
